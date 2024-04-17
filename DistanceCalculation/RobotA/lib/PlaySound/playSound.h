1296fbb608b2b18ee256a1f4b537a8c9eb53eb5b
xrgman <xrgman@gmail.com>
2024-04-17 09:51:54 +0200

    Added distance calculations part to repository

---
+++ DistanceCalculation/RobotA/lib/PlaySound/playSound.h
@@ -0,0 +1,41 @@
+#ifndef PLAYSOUND_H
+#define PLAYSOUND_H
+
+#include "mbed.h"
+
+Timer outputTimer;
+bool playingSound = false;
+
+/// @brief Play a beep over the speaker.
+/// @param speaker PWM object of the speaker output.
+/// @return Whether or not playing is done.
+bool playBeep(PwmOut &speaker)
+{
+  if (!playingSound)
+  {
+    //speaker.write(0.5f);
+
+    outputTimer.reset();
+    outputTimer.start();
+
+    playingSound = true;
+
+    return false;
+  }
+
+  auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(outputTimer.elapsed_time()).count();
+
+  if (elapsedTime >= 500)
+  {
+    //speaker.write(0.0f);
+
+    outputTimer.stop();
+    playingSound = false;
+
+    return true;
+  }
+
+  return false;
+}
+
+#endif
